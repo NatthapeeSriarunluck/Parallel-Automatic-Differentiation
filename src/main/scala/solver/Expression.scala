4da9132a549edc211a10fccd5f740ee14da694f1
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-07-03 02:12:09 +0700

    fix: added a variable parameter to evaluateAndDerive in order to specify what to differentiate in respect to

--- src/main/scala/solver/Expression.scala
+++ src/main/scala/solver/Expression.scala
@@ -1,6 +1,9 @@
 package solver
+
+import com.sun.org.apache.xpath.internal.operations.Variable
+
 sealed trait Expression {
-  def evaluateAndDerive(varAssn: Map[String, Double]): ValueAndPartial
+  def evaluateAndDerive(varAssn: Map[String, Double], variable: String): ValueAndPartial
 }
 
 case class ValueAndPartial(value: Double, partial: Double) {
@@ -11,7 +14,8 @@ case class Constant(n: Double) extends Expression {
   override def toString = s"Const($n)"
 
   override def evaluateAndDerive(
-      varAssn: Map[String, Double]
+      varAssn: Map[String, Double],
+      variable: String
   ): ValueAndPartial = {
     ValueAndPartial(n, 0)
   }
@@ -21,10 +25,10 @@ case class Var(name: String) extends Expression {
   override def toString = s"Var($name)"
 
   override def evaluateAndDerive(
-      varAssn: Map[String, Double]
+      varAssn: Map[String, Double], variable: String
   ): ValueAndPartial = {
     val value = varAssn(name)
-    val partial = 1.0
+    val partial = if (name == variable) 1 else 0
     ValueAndPartial(value, partial)
   }
 }
@@ -36,10 +40,11 @@ case class Sum(e1: Expression, e2: Expression) extends Expression {
   }
 
   override def evaluateAndDerive(
-      varAssn: Map[String, Double]
+      varAssn: Map[String, Double],
+      variable: String
   ): ValueAndPartial = {
-    val ValueAndPartial(valueA, partialA) = e1.evaluateAndDerive(varAssn)
-    val ValueAndPartial(valueB, partialB) = e2.evaluateAndDerive(varAssn)
+    val ValueAndPartial(valueA, partialA) = e1.evaluateAndDerive(varAssn, variable)
+    val ValueAndPartial(valueB, partialB) = e2.evaluateAndDerive(varAssn, variable)
     ValueAndPartial(valueA + valueB, partialA + partialB)
 
   }
@@ -52,10 +57,11 @@ case class Prod(e1: Expression, e2: Expression) extends Expression {
     s"Prod($l, $r)"
   }
   override def evaluateAndDerive(
-      varAssn: Map[String, Double]
+      varAssn: Map[String, Double],
+      variable: String
   ): ValueAndPartial = {
-    val ValueAndPartial(valueA, partialA) = e1.evaluateAndDerive(varAssn)
-    val ValueAndPartial(valueB, partialB) = e2.evaluateAndDerive(varAssn)
+    val ValueAndPartial(valueA, partialA) = e1.evaluateAndDerive(varAssn, variable)
+    val ValueAndPartial(valueB, partialB) = e2.evaluateAndDerive(varAssn, variable)
     ValueAndPartial(valueA * valueB, valueB * partialA + valueA * partialB)
   }
 }
@@ -66,10 +72,11 @@ case class Power(b: Expression, e: Expression) extends Expression {
     s"Power($bb, $ee)"
   }
   override def evaluateAndDerive(
-      varAssn: Map[String, Double]
+      varAssn: Map[String, Double],
+      variable: String
   ): ValueAndPartial = {
-    val ValueAndPartial(valueB, partialB) = b.evaluateAndDerive(varAssn)
-    val ValueAndPartial(valueE, partialE) = e.evaluateAndDerive(varAssn)
+    val ValueAndPartial(valueB, partialB) = b.evaluateAndDerive(varAssn, variable)
+    val ValueAndPartial(valueE, partialE) = e.evaluateAndDerive(varAssn, variable)
     val value = Math.pow(valueB, valueE)
     val partial = value * (valueE / valueB * partialB + Math.log(valueB) * partialE)
     ValueAndPartial(value, partial)
