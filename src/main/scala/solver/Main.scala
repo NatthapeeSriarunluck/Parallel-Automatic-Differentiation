b4554ae80e8635ff73bf118e64206979d05ba178
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-07-04 01:57:51 +0700

    feat: added a main method to support backwards accumulation

--- src/main/scala/solver/Main.scala
+++ src/main/scala/solver/Main.scala
@@ -1,5 +1,7 @@
 package solver
 
+package solver
+
 object Main {
   def main(args: Array[String]): Unit = {
     val expString = "x * (x + y) + y * y"
@@ -8,13 +10,26 @@ object Main {
     val varAssn = varNames.zip(varValues).toMap
 
     try {
+      //      val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+      //      val xPartial = exp.evaluateAndDerive(Map("x" -> varAssn("x"), "y" -> varAssn("y")), "x").partial
+      //      val yPartial = exp.evaluateAndDerive(Map("x" -> varAssn("x"), "y" -> varAssn("y")), "y").partial
+      //      println(s"The partial derivative with respect to x at the point $varAssn is $xPartial")
+      //      println(s"The partial derivative with respect to y at the point $varAssn is $yPartial")
+
       val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
-      val xPartial = exp.evaluateAndDerive(Map("x" -> varAssn("x"), "y" -> varAssn("y")), "x").partial
-      val yPartial = exp.evaluateAndDerive(Map("x" -> varAssn("x"), "y" -> varAssn("y")), "y").partial
-      println(s"The partial derivative with respect to x at the point $varAssn is $xPartial")
-      println(s"The partial derivative with respect to y at the point $varAssn is $yPartial")
-    } catch {
+      val evaluated = Process.eval(exp, varAssn)
+      println(s"The expression $expString at the point $varAssn = $evaluated")
+
+      // call the derive method
+      exp.derive(1)
+
+      // print the final partial derivative  solved
+      println(s"The partial derivative with respect to x at the point $varAssn is ${exp.partial}")
+    }
+
+    catch {
       case e: Exception => println(s"Error: ${e.getMessage}")
     }
   }
 }
+
