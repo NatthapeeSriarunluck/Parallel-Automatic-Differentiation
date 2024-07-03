59d066d92ae4a14f819fa0afc37dbf755cb9342f
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-07-04 01:57:17 +0700

    feat: added a derive method with seed to support backwards accumulation

--- src/main/scala/solver/Expression.scala
+++ src/main/scala/solver/Expression.scala
@@ -3,7 +3,12 @@ package solver
 import com.sun.org.apache.xpath.internal.operations.Variable
 
 sealed trait Expression {
+  var value: Double = 0
+  var partial: Double = 0
+
   def evaluateAndDerive(varAssn: Map[String, Double], variable: String): ValueAndPartial
+
+  def derive(seed: Double): Unit
 }
 
 case class ValueAndPartial(value: Double, partial: Double) {
@@ -14,10 +19,14 @@ case class Constant(n: Double) extends Expression {
   override def toString = s"Const($n)"
 
   override def evaluateAndDerive(
-      varAssn: Map[String, Double],
-      variable: String
-  ): ValueAndPartial = {
+                                  varAssn: Map[String, Double],
+                                  variable: String
+                                ): ValueAndPartial = {
     ValueAndPartial(n, 0)
+  }
+
+  override def derive(seed: Double): Unit = {
+    partial += 0
   }
 }
 
@@ -25,11 +34,16 @@ case class Var(name: String) extends Expression {
   override def toString = s"Var($name)"
 
   override def evaluateAndDerive(
-      varAssn: Map[String, Double], variable: String
-  ): ValueAndPartial = {
-    val value = varAssn(name)
-    val partial = if (name == variable) 1 else 0
+                                  varAssn: Map[String, Double], variable: String
+                                ): ValueAndPartial = {
+    value = varAssn(name)
+    partial = if (name == variable) 1 else 0
     ValueAndPartial(value, partial)
+  }
+
+
+  override def derive(seed: Double): Unit = {
+    partial += seed
   }
 }
 
@@ -40,13 +54,18 @@ case class Sum(e1: Expression, e2: Expression) extends Expression {
   }
 
   override def evaluateAndDerive(
-      varAssn: Map[String, Double],
-      variable: String
-  ): ValueAndPartial = {
+                                  varAssn: Map[String, Double],
+                                  variable: String
+                                ): ValueAndPartial = {
     val ValueAndPartial(valueA, partialA) = e1.evaluateAndDerive(varAssn, variable)
     val ValueAndPartial(valueB, partialB) = e2.evaluateAndDerive(varAssn, variable)
     ValueAndPartial(valueA + valueB, partialA + partialB)
 
+  }
+
+  override def derive(seed: Double): Unit = {
+    e1.derive(seed)
+    e2.derive(seed)
   }
 }
 
@@ -56,13 +75,19 @@ case class Prod(e1: Expression, e2: Expression) extends Expression {
 
     s"Prod($l, $r)"
   }
+
   override def evaluateAndDerive(
-      varAssn: Map[String, Double],
-      variable: String
-  ): ValueAndPartial = {
+                                  varAssn: Map[String, Double],
+                                  variable: String
+                                ): ValueAndPartial = {
     val ValueAndPartial(valueA, partialA) = e1.evaluateAndDerive(varAssn, variable)
     val ValueAndPartial(valueB, partialB) = e2.evaluateAndDerive(varAssn, variable)
     ValueAndPartial(valueA * valueB, valueB * partialA + valueA * partialB)
+  }
+
+  override def derive(seed: Double): Unit = {
+    e1.derive(e2.value * seed)
+    e2.derive(e1.value * seed)
   }
 }
 
@@ -71,14 +96,20 @@ case class Power(b: Expression, e: Expression) extends Expression {
     val (bb, ee) = (b.toString, e.toString)
     s"Power($bb, $ee)"
   }
+
   override def evaluateAndDerive(
-      varAssn: Map[String, Double],
-      variable: String
-  ): ValueAndPartial = {
+                                  varAssn: Map[String, Double],
+                                  variable: String
+                                ): ValueAndPartial = {
     val ValueAndPartial(valueB, partialB) = b.evaluateAndDerive(varAssn, variable)
     val ValueAndPartial(valueE, partialE) = e.evaluateAndDerive(varAssn, variable)
     val value = Math.pow(valueB, valueE)
     val partial = value * (valueE / valueB * partialB + Math.log(valueB) * partialE)
     ValueAndPartial(value, partial)
+  }
+
+  override def derive(seed: Double): Unit = {
+    b.derive(e.value * Math.pow(b.value, e.value - 1) * seed)
+    e.derive(Math.pow(b.value, e.value) * Math.log(b.value) * seed)
   }
 }
