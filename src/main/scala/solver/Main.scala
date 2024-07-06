c909e7b8c77e562e9059ca60c6b2db2380d6a1bd
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-07-07 03:36:16 +0700

    feat: added reverse mode test case

--- src/main/scala/solver/Main.scala
+++ src/main/scala/solver/Main.scala
@@ -1,7 +1,5 @@
 package solver
 
-package solver
-
 object Main {
   def main(args: Array[String]): Unit = {
     val expString = "x * (x + y) + y * y"
@@ -10,23 +8,30 @@ object Main {
     val varAssn = varNames.zip(varValues).toMap
 
     try {
-      //      val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
-      //      val xPartial = exp.evaluateAndDerive(Map("x" -> varAssn("x"), "y" -> varAssn("y")), "x").partial
-      //      val yPartial = exp.evaluateAndDerive(Map("x" -> varAssn("x"), "y" -> varAssn("y")), "y").partial
-      //      println(s"The partial derivative with respect to x at the point $varAssn is $xPartial")
-      //      println(s"The partial derivative with respect to y at the point $varAssn is $yPartial")
 
-      val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
-      val evaluated = Process.eval(exp, varAssn)
-      println(s"The expression $expString at the point $varAssn = $evaluated")
+//      val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+//      // Forward Mode
+//      println("Forward Mode")
+//
+//      val evaluated = Process.eval(exp, varAssn)
+//      println(s"The value of the expression at the point $varAssn is $evaluated")
+//
+//      val xPartial = AutoDiff.forwardMode(exp, varAssn, "x")
+//      val yPartial = AutoDiff.forwardMode(exp, varAssn, "y")
+//      println(s"The partial derivative with respect to x at the point $varAssn is $xPartial")
+//      println(s"The partial derivative with respect to y at the point $varAssn is $yPartial")
 
-      // call the derive method
-      exp.derive(1)
-
-      // print the final partial derivative  solved
-      println(s"The partial derivative with respect to x at the point $varAssn is ${exp.partial}")
+      // Reverse Mode
+      println("Reverse Mode")
+      val exp2 = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+      AutoDiff.reverseMode(exp2, varAssn)
+      val xVar = exp2.findVar("x").getOrElse(throw new Exception("Variable not found"))
+      val yVar = exp2.findVar("y").getOrElse(throw new Exception("Variable not found"))
+      val xGrad = Expression.grads.getOrElse("x", 0.0)
+      val yGrad = Expression.grads.getOrElse("y", 0.0)
+      println(s"The partial derivative with respect to x at the point $varAssn is $xGrad")
+      println(s"The partial derivative with respect to y at the point $varAssn is $yGrad")
     }
-
     catch {
       case e: Exception => println(s"Error: ${e.getMessage}")
     }
