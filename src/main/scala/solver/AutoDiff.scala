c07794309eaea9af07c2735e10b71e6401e5d787
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-07-07 03:33:16 +0700

    feat: added AutoDiff class to specify foward and reverse mode

---
+++ src/main/scala/solver/AutoDiff.scala
@@ -0,0 +1,17 @@
+package solver
+
+object AutoDiff{
+  def forwardMode(e: Expression, varAssn: Map[String, Double], variable: String): ValueAndPartial = {
+    e.evaluateAndDerive(varAssn, variable)
+  }
+
+  def reverseMode(expr: Expression, varAssn: Map[String, Double]): Unit = {
+    // Initialize the values of the expressions
+    val evaluated = Process.eval(expr, varAssn)
+
+    println("Value of the expression: " + evaluated)
+    // Start backward pass from the output
+    expr.backward(1, varAssn)
+  }
+
+}
