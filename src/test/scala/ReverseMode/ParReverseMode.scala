dd222d1478394c53961438e466b4652d8e55f8ec
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-07-16 02:06:47 +0700

    fix: refactored all tests to reduce duplicates

---
+++ src/test/scala/ReverseMode/ParReverseMode.scala
@@ -0,0 +1,31 @@
+package ReverseMode
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funsuite.AnyFunSuite
+import parallel_ad.{Par_AutoDiff, PartialDerivativeOf, ValueOf}
+
+class ParReverseMode extends AnyFunSuite with BeforeAndAfterEach {
+  override def beforeEach(): Unit = {
+    PartialDerivativeOf.grads.clear()
+    ValueOf.values.clear()
+    super.beforeEach() // To be stackable, must call super.beforeEach()
+  }
+
+  def testReverseModeAutoDiffs(testName: String, expression: String, varNames: List[String], varValues: List[Double], expected: Map[String, Double]): Unit = {
+    val varAssn = varNames.zip(varValues).toMap
+    test(s"$testName (Par_AutoDiff)") {
+      val result = Par_AutoDiff.reverseMode(expression, varAssn)
+      expected.foreach { case (variable, value) =>
+        assert(result(variable) == value)
+      }
+    }
+  }
+
+  testReverseModeAutoDiffs("check differentiation of x * x at x = 2", "x * x", List("x"), List(2.0), Map("x" -> 4.0))
+  testReverseModeAutoDiffs("check differentiation of x + y at x = 1, y = 2", "x + y", List("x", "y"), List(1.0, 2.0), Map("x" -> 1.0, "y" -> 1.0))
+  testReverseModeAutoDiffs("check differentiation of x * y at x = 2, y = 3", "x * y", List("x", "y"), List(2.0, 3.0), Map("x" -> 3.0, "y" -> 2.0))
+  testReverseModeAutoDiffs("check differentiation of cos(x) at x = 0", "cos(x)", List("x"), List(0.0), Map("x" -> 0.0))
+  testReverseModeAutoDiffs("check differentiation of ln(x) at x = 1", "ln(x)", List("x"), List(1.0), Map("x" -> 1.0))
+  testReverseModeAutoDiffs("check differentiation of x^y at x = 2, y = 3", "x^y", List("x", "y"), List(2.0, 3.0), Map("x" -> 12.0, "y" -> (Math.pow(2.0, 3.0) * Math.log(2.0))))
+  testReverseModeAutoDiffs("check differentiation of x * (x + y) + y * y at x = 2, y = 3", "x * (x + y) + y * y", List("x", "y"), List(2.0, 3.0), Map("x" -> 7.0, "y" -> 8.0))
+}
---
+++ src/test/scala/parser_test/ParserTests.scala
@@ -0,0 +1,29 @@
+
+package parser_test
+
+import org.scalatest.funsuite.AnyFunSuite
+import sequential_ad.Parser
+import parallel_ad.Par_Parser
+
+class ParserTests extends AnyFunSuite {
+
+  def testParsers(testName: String, expression: String, expected: String): Unit = {
+    test(s"$testName (sequential)") {
+      val exp = Parser(expression).getOrElse(throw new Exception("Invalid expression"))
+      assert(exp.toString == expected)
+    }
+
+    test(s"$testName (parallel)") {
+      val exp = Par_Parser(expression).getOrElse(throw new Exception("Invalid expression"))
+      assert(exp.toString == expected)
+    }
+  }
+
+  testParsers("sin expression", "sin(x)", "Sin(Var(x))")
+  testParsers("cos expression", "cos(x)", "Cos(Var(x))")
+  testParsers("tan expression", "tan(x)", "Tan(Var(x))")
+  testParsers("mixed trig expression", "sin(x) + cos(x) + tan(x)", "Sum(Sum(Sin(Var(x)),Cos(Var(x))),Tan(Var(x)))")
+  testParsers("ln expression", "ln(x)", "Ln(Var(x))")
+  testParsers("power expression", "x^2", "Power(Var(x),Constant(2.0))")
+  testParsers("xy", "xy", "Prod(Var(x),Var(y))")
+}
