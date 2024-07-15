2aefd94663bdd89304a67a8e9c8551a92a6de29e
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-07-16 00:35:06 +0700

    feat: added tests for parser

---
+++ src/test/scala/parser_test/SeqParserTests.scala
@@ -0,0 +1,52 @@
+package parser_test
+
+import org.scalatest.funsuite.AnyFunSuite
+import sequential_ad.Parser
+class SeqParserTests extends AnyFunSuite:
+
+  test("sin expression" ) {
+    val expString = "sin(x)"
+    val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+    assert(exp.toString == "Sin(Var(x))")
+  }
+
+  test("cos expression") {
+    val expString = "cos(x)"
+    val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+    assert(exp.toString == "Cos(Var(x))")
+  }
+
+  test("tan expression") {
+
+    val expString = "tan(x)"
+    val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+    assert(exp.toString == "Tan(Var(x))")
+  }
+
+  test("mixed trig expression") {
+    val expString = "sin(x) + cos(x) + tan(x)"
+    val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+    assert(exp.toString ==   "Sum(Sum(Sin(Var(x)),Cos(Var(x))),Tan(Var(x)))")
+  }
+
+  test("ln expression") {
+    val expString = "ln(x)"
+    val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+    assert(exp.toString == "Ln(Var(x))")
+  }
+
+  test("power expression") {
+    val expString = "x^2"
+    val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+    assert(exp.toString == "Power(Var(x),Constant(2.0))")
+  }
+
+  test("xy") {
+    val expString = "xy"
+    val exp = Parser(expString).getOrElse(throw new Exception("Invalid expression"))
+    assert(exp.toString == "Prod(Var(x),Var(y))")
+  }
+
+
+
+
